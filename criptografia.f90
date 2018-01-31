709e1b4d927541f95b30dd7e8f12798d80ac3ffb
javiyo <javierestegracia@gmail.com>
2018-01-31 19:04:41 +0100

    Update criptografia.f90

--- criptografia.f90
+++ criptografia.f90
@@ -10,12 +10,8 @@ contains
 	
 	function numletra(n)
 	integer, intent(in) :: n
-	character :: numletra   
-	if ((n+96)==0) then
-		numletra=("z")
-	else				  !Funcion inversa de la anterior, achar() nos devuelve una letra al meter un entero
-	numletra=achar(n+96)
-	end if 				  !Tenemos que deshacer el -96 de antes
+	character :: numletra   		  !Funcion inversa de la anterior, achar() nos devuelve una letra al meter un entero
+	numletra=achar(n+96)			  !Tenemos que deshacer el -96 de antes
 	end function
 
 
@@ -40,7 +36,8 @@ contains
 	integer, intent(in) :: a,k,b
 	integer :: afin                 !Afin (realiza el multiplicativo y le suma el aditivo)	
 	afin=mod(k*a+b,26)
-	end function afin
+	if (afin==0) afin=26 !Cuando k*a+b sea igual a 26 el resto es 0, por lo que esta funcion nunca nos devuelve el numero 26
+	end function afin    !para asignarlo a la z, tendremos que cambiar el 0 por la z manualmente
 
 	function rkaditivo(k)
 	integer,intent(in) :: k
