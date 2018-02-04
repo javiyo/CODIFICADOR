f07cd98cb9f7739e31dba97180643239d65e3454
javiyo <javierestegracia@gmail.com>
2018-02-04 16:44:23 +0100

    Update criptografia.f90

--- criptografia.f90
+++ criptografia.f90
@@ -4,14 +4,21 @@ contains
 
 	function letranum(a) !Funcion que traduce de las letras al codigo
 	character, intent(in) :: a
-	integer :: var,letranum
-	letranum=iachar(a) - 96 !Funciona gracias a la funcion iachar() que nos devuelve el valor ASCII de cada letra
-	end function		!El -96 es unicamente para que sea acorde a los numeros que nos proporciona fernando en la tabla 
+	integer :: letranum, i
+	character,dimension(26) :: v
+	v=(/"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"/)
+	do i=1,26
+		if (a==v(i)) letranum=i
+	end do
 	
+	end function		
+
 	function numletra(n)
 	integer, intent(in) :: n
-	character :: numletra   		  !Funcion inversa de la anterior, achar() nos devuelve una letra al meter un entero
-	numletra=achar(n+96)			  !Tenemos que deshacer el -96 de antes
+	character :: numletra   		  
+	character,dimension(26) :: v
+	v=(/"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"/)
+	numletra=v(n)
 	end function
 
 
@@ -36,7 +43,7 @@ contains
 	function afin(a,k,b)
 	integer, intent(in) :: a,k,b
 	integer :: afin                 !Afin (realiza el multiplicativo y le suma el aditivo)	
-	afin=mod(k*a+b,26)
+	afin=mod((k*a)+b,26)
 	if (afin==0) afin=26 !Cuando k*a+b sea igual a 26 el resto es 0, por lo que esta funcion nunca nos devuelve el numero 26
 	end function afin    !para asignarlo a la z, tendremos que cambiar el 0 por la z manualmente
 
